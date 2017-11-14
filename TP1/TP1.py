952e2ecc7808db5df55a34d823091f5b563d2da1
aissaoui <ramez.aissaoui@devinci.fr>
2017-11-14 13:26:21 +0100

    Question 1, 2, 3 and 5

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 
 #Question1
 with open('jfkrelease-2017-dce65d0ec70a54d5744de17d280f3ad2.csv','r') as filejfk:
@@ -16,26 +17,55 @@ with open('jfkrelease-2017-dce65d0ec70a54d5744de17d280f3ad2.csv','r') as filejfk
 #Question2
 #Compute the mean number of pages per document, the minimum and maximum of pages per document
 
-somme=0
-total=0
+#with open('jfkrelease-2017-dce65d0ec70a54d5744de17d280f3ad2.csv','r') as filejfk:
+#reader = csv.reader(filejfk, delimiter=':', quoting=csv.QUOTE_NONE)
+filejfk = open('jfkrelease-2017-dce65d0ec70a54d5744de17d280f3ad2.csv','r')
+
+liste = list(filejfk)
+nom = liste[0].split(';')
+nbField = len(nom)
+count=0
+
 mean=0
+min=999
 max=0
-min=0
+count=0
+missing = 0
+nbPages=0
+sum=0
+col=nom.index('Num Pages')
+nbDocs=0
+for line in liste:
+	nbDocs+=1
+	field = line.split(';')
+	try :
+		nbPages=int(field[col])
+		sum=int(field[col])+sum
+		if nbPages<min:
+			min=nbPages
+		if nbPages>max:
+			max=nbPages
+	except ValueError:
+		missing += 1
+mean=sum/nbDocs
+print("La moyenne est ", mean)
+print("La valeur minimum est ",min)
+print("La valeur maximum", max)
+print("Le nombre de valeurs manquantes est", missing)
 
-with open('jfkrelease-2017-dce65d0ec70a54d5744de17d280f3ad2.csv','r') as filejfk:
-    reader = csv.reader(filejfk, delimiter=':', quoting=csv.QUOTE_NONE)
 
-    for row in reader:
-        field=row
-        #print(field)
-        nbpages=list(field)
-        #print(nbpages)
-        #print(type(nbpages))
-        #print(len(nbpages))
+#Question3
+#doc type =7-1
 
-import pandas as pd
+type=nom.index('Doc Type')
+Doctype = field[type]
 
-df = pd.read_csv('jfkrelease-2017-dce65d0ec70a54d5744de17d280f3ad2.csv',na_values=['.'],error_bad_lines=False,sep=";")
+for line in [1,liste]:
+	field = line.split(';'); Doctype = field[type]
+    #print(Doctype)
+	try :
+		if field[type]!=Doctype : print(field[type])
+	except ValueError:
+		missing += 1
 
 
-print(df.ix[:, 11])
