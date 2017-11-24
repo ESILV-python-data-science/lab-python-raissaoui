73d1b0778188b4372782392fec588c28ae672998
aissaoui <ramez.aissaoui@devinci.fr>
2017-11-24 14:14:51 +0100

    Add pages and Question 1

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -1,4 +1,7 @@
 import logging
 
+# Question 1 : no similar pages
 
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
+# logging.basicConfig(filename='example.log',level=logging.DEBUG)
+
+
